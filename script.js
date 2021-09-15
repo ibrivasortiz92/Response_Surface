c7385c6cfcb10586f7d807a4cb56518a6475f4f5
ibrivasortiz92 <78561871+ibrivasortiz92@users.noreply.github.com>
2021-09-15 14:46:36 -0300

    some mods

--- script.js
+++ script.js
@@ -25,47 +25,166 @@ document.getElementById("calculate").addEventListener("click",()=>{
   let xdata = x_layout(xmin, xmax);
   let ydata = y_layout(ymin, ymax);
   let zdata = z_data(a0, a1, a2, a3, a4, a5, xdata, ydata);
-  let plot_color = "YlGnBu";
+  let plot_color = "RdBu", rev = false;
   if(document.getElementById("gray_color").checked){
     plot_color = "Greys";
+    rev = true;
   }
 
-  Plotly.newPlot("myplot", [{
-    x: xdata,
-    y: ydata,
-    z: zdata,
-    type: "surface",
-    colorscale: plot_color,
-    contours: {
-      z: {
-        show: true,
-        usecolormap: true,
-        highlightcolor: "#42f462",
-        project: {z: true}
+  if(document.getElementById("proy_map").checked){
+    
+    Plotly.newPlot("myplot", [
+      // DATA
+      {
+      x: xdata,
+      y: ydata,
+      z: zdata,
+      type: "contour",
+      colorscale: plot_color,
+      reversescale: rev,
+      contours: {
+        coloring: 'heatmap',
+        showlabels: true,
+        labelfont: {
+          family: 'Raleway',
+          size: 12,
+          color: 'white',
+        }
+      },
+      cmin: 0,
+      colorbar: {
+        title: ztitle,
+        titleside: "right",
+        thickness: 10,
+        len: 0.8
       }
-    }
-  }], {
-    scene: {
+    }], 
+
+    // LAYOUT
+    {
+      margin: {
+        t: 50,
+        b: 50
+      },
       xaxis: {title: xtitle},
       yaxis: {title: ytitle},
-      zaxis: {
-        title: ztitle,
-        rangemode: 'tozero',
-        autorange: true
+      width: 700,
+      height: 500
+    }, 
+    // CONFIG
+    {
+      responsive: true,
+      displaylogo: false,
+      toImageButtonOptions: {
+        format: 'png',
+        filename: 'response_surface',
+        height: 500,
+        width: 700,
+        scale: 1
       }
-    },
-    width: 500,
-    height: 500
-  }, {
-    displaylogo: false,
-    toImageButtonOptions: {
-      format: 'png',
-      filename: 'response_surface',
-      height: 700,
-      width: 900,
-      scale: 2
-    }
-  });
+    });
+
+  }
+  else {
+
+    Plotly.newPlot("myplot", [
+
+      // DATA
+      {
+      x: xdata,
+      y: ydata,
+      z: zdata,
+      type: "surface",
+      colorscale: plot_color,
+      reversescale: rev,
+      contours: {
+        z: {
+          show: true,
+          usecolormap: true,
+          highlightcolor: "#42f462",
+          highlightwidth: 20,
+          width: 20,
+          start: 5,
+          end: 10,
+          project: {z: true},
+        },
+        coloring: 'heatmap',
+      showlabels: true,
+      labelfont: {
+        family: 'Raleway',
+        size: 12,
+        color: 'white',
+      }
+      },
+      cmin: 0,
+      colorbar: {
+        title: ztitle,
+        titleside: "right",
+        thickness: 10,
+        len: 0.8
+      }
+    }], 
+
+    // LAYOUT
+    {
+      margin: {
+        t: 0,
+        b: 0
+      },
+      scene: {
+        xaxis: {
+          title: xtitle, 
+          showgrid: true, 
+          showline: true, 
+          mirror: true,
+          zeroline: false,
+          gridcolor: "rgb(240,240,240)"
+        },
+        yaxis: {
+          title: ytitle, 
+          showgrid: true, 
+          showline: true, 
+          mirror: true,
+          zeroline: false,
+          gridcolor: "rgb(240,240,240)"
+        },
+        zaxis: {
+          title: ztitle,
+          rangemode: 'tozero',
+          autorange: true,
+          showgrid: true, 
+          showline: true, mirror: true, 
+          showspikes: true,
+          zeroline: false,
+          gridcolor: "rgb(240,240,240)"
+        },
+        camera: {
+          type: "orthographic",
+          eye: {
+            x: 1.75, y: 1.75, z: 1.75
+          }
+        }
+      },
+      width: 700,
+      height: 500
+    }, 
+
+    // CONFIG
+    {
+      responsive: true,
+      displaylogo: false,
+      toImageButtonOptions: {
+        format: 'png',
+        filename: 'response_surface',
+        height: 500,
+        width: 700,
+        scale: 1
+      }
+    });
+
+  }
+
+  
 });
 
 
